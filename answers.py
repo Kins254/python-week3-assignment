39c9d006f1d1f0d87d60985817abd3870de8ecdd
Erick <erickgitau250@gmail.com>
2024-11-21 20:38:38 +0300

    Assignment submission

---
+++ answers.py
@@ -0,0 +1,22 @@
+#Question 1: Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. The function should take the original price (price) and the discount percentage (discount_percent) as parameters. If the discount is 20% or higher, apply the discount; otherwise, return the original price.
+
+ 
+def calculate_discount(price,discount_percent):
+    if discount_percent >= 20 :
+        results=price*((100-discount_percent)/100)
+        print(f"The price after discount is {results}")
+        
+    else:
+        results=price
+        print(f"The price for the item is: {results}")    
+        
+calculate_discount(1000,10)    
+
+
+#Question 2:Using the calculate_discount function, prompt the user to enter the original price of an item and the discount percentage. Print the final price after applying the discount, or if no discount was applied, print the original price    
+          
+price=float(input("Please Enter the price of the item:"))
+discount_percent=float(input("Please Enter the discount percent of the item:")) 
+#Call the function
+calculate_discount(price,discount_percent)    
+         
